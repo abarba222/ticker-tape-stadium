965d0ebd36a509b1c69bd16692cc27673ecceea5
Anthony Barba <abarba222@gmail.com>
2015-07-11 18:15:16 -0400

    designed the top-stocks page

--- app/models/team.js
+++ app/models/team.js
@@ -4,5 +4,7 @@ export default DS.Model.extend({
   Teams: DS.attr('string'),
   symbol: DS.attr('string'),
   value: DS.attr('number'),
-  streak: DS.attr('number')
+  streak: DS.attr('number'),
+  rank: DS.attr('number'),
+  Name: DS.attr('string')
 });
