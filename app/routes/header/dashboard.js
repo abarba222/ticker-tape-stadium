07b11aefea9653073a19c0fac443b5dadee6a515
Anthony Barba <abarba222@gmail.com>
2015-07-14 03:28:21 -0400

    added functionallity to the dashboard and stocks pages plus styling.

--- app/routes/header/dashboard.js
+++ app/routes/header/dashboard.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     //return Ember.RSVP.hash({
-      return this.store.findAll('team', params.length=5),
+      return this.store.findAll('team', params.length=5);
 
-      this.store.findAll('match', params.length=5);
+      //this.store.findAll('match', params.length=5);
   //  });
   }
 });
